54b4666ac65e83f8f50fd68647a9c0e672fc9aa8
simarg42 <kovalevmaksim54@yanex.ru>
2023-03-26 18:52:28 +0300

    Добавление метода минимализации ДКА

--- Lab1/DKA/src/main/kotlin/ru/bauman/kompil/machine/DFA.kt
+++ Lab1/DKA/src/main/kotlin/ru/bauman/kompil/machine/DFA.kt
@@ -2,6 +2,7 @@ package ru.bauman.kompil.machine
 
 import ru.bauman.kompil.algorithm.ReversePolishNotation
 import ru.bauman.kompil.tree.SyntaxTree
+import java.util.Arrays
 import kotlin.math.abs
 
 class DFA(var stateDiagram: StateDiagram) {
@@ -31,35 +32,99 @@ class DFA(var stateDiagram: StateDiagram) {
 
     private fun itmoMinimization() {
         //Шаг 1: Таблица из обратных ребер
-        val reverseEdgeList = mutableListOf<MutableList<Int>>()
-        for (i in 0 until stateDiagram.table.size) {
-            for (a in stateDiagram.abc) {
-                reverseEdgeList.add(findReverseEdge(i, a))
+        //Вектор d-1: (Номер состояния, номер символа) = (номера состояний)
+        val reverseEdgeList = mutableMapOf<List<Int>, MutableSet<Int>>()
+        for (strNum in 0 until stateDiagram.table.size) { //Идем по состояниям
+            for (sym in stateDiagram.abc) {   //Берем символ из алфавита
+                val listNumSym = listOf(strNum, stateDiagram.abc.indexOf(sym))
+                val numStates = findReverseEdge(strNum, sym);
+                reverseEdgeList[listNumSym] = numStates
             }
         }
+
 
         //Шаг 2: Массив достижимости состояний из стартоого
         val reachable = reachableState(0)
 
         //Шаг 3 и 4
-        val marked = buildEqTable(reverseEdgeList)
+        val markedTable = buildEqTable(reverseEdgeList)
+
+        //Шаг 5:
+        val n = stateDiagram.states.size
+        val component = Array(n) { -1 }
+        for (i in 0 until n)
+            if (!markedTable[0][i])
+                component[i] = 0
+        var componentCounts = 0
+        for (i in 1 until n) {
+            if(!reachable[i])
+                continue
+            if(component[i] == -1) {
+                componentCounts++
+                component[i] = componentCounts
+                for(j in i + 1 until n)
+                    if(!markedTable[i][j])
+                        component[j] = componentCounts
+            }
+        }
+
+        component.forEach { print("$it \t") }
+/*        println("Маркированная таблица: ")
+        for (i in markedTable) {
+            for (j in i)
+                print("$j \t")
+        println()
+        }
+
+        for (s in stateDiagram.states) {
+            s.forEach { print("$it \t") }
+            println()
+        }*/
     }
 
-    private fun buildEqTable(reverseEdgeList: MutableList<MutableList<Int>>): Any {
-        return false
+    //Построение таблицы, нейминг сохранен
+    private fun buildEqTable(reverseEdgeList: MutableMap<List<Int>, MutableSet<Int>>): Array<BooleanArray> {
+        val Q = ArrayDeque<List<Int>>()
+        val n = stateDiagram.states.size
+        //!!! Работает только для 1 конечного состояния
+        val isTerminal = BooleanArray(n) {false}
+        for ((m, st) in stateDiagram.states.withIndex())
+            for (l in stateDiagram.endedNumber)
+                if (st.contains(l))
+                    isTerminal[m] = true
+        val marked = Array(n) { BooleanArray(n) {false} } //Лямбды хороши
+        //Шаг 3
+        for (i in 0 until n)
+            for (j in 0 until n)
+                if(!marked[i][j] && (isTerminal[i] != isTerminal[j])) {
+                    marked[i][j] = true
+                    marked[j][i] = true
+                    Q.addLast(listOf(i, j))
+                }
+        //Шаг 4
+        while(Q.isNotEmpty()) {
+            val mapUV = Q.removeFirst()
+            for (c in 0 until stateDiagram.abc.size)
+                for (r in reverseEdgeList[listOf(mapUV[0], c)]!!)
+                    for (s in reverseEdgeList[listOf(mapUV[1], c)]!!)
+                        if (!marked[r][s]) {
+                            marked[r][s] = true
+                            marked[s][r] = true
+                            Q.addLast(listOf(r, s))
+                        }
+        }
+
+        return marked
     }
 
-    //Вычисление вектора конкретного d-1
-    private fun findReverseEdge(stateNumber: Int, a: String): MutableList<Int> {
-        val reverseEdges = mutableListOf<Int>()
-        for (str in stateDiagram.table) {
-            println(stateDiagram.abc)
-            println(a)
-            println(stateDiagram.abc.indexOf(a))
+
+    //Вычисление вектора конкретного d-1 с родителями
+    private fun findReverseEdge(stateNumber: Int, a: String): MutableSet<Int> {
+        val reverseEdges = mutableSetOf<Int>()
+        for ((k, str) in stateDiagram.table.withIndex()) {
             val pos = stateDiagram.abc.indexOf(a)
-            println(pos)
             if (str[pos] == stateNumber)
-                reverseEdges.add(stateDiagram.table.indexOf(str))
+                reverseEdges.add(k)
         }
         return reverseEdges
     }
@@ -76,7 +141,8 @@ class DFA(var stateDiagram: StateDiagram) {
             return true
         else {
             checkedStates.add(checkedStateNumber)
-            val uncheckedStates = stateDiagram.table[checkedStateNumber]
+            val uncheckedStates = mutableListOf<Int>()
+            uncheckedStates.addAll(stateDiagram.table[checkedStateNumber])
             uncheckedStates.remove(checkedStateNumber) //Проверяем все кроме текущего
             var checkResult = false
             for (unS in uncheckedStates) {
