52c32477441eda751db2d11978561c824aa123d8
martel42 <kovalevmaksim54@yandex.ru>
2023-03-31 18:13:14 +0300

    Вводить множество цепочек

--- Lab1/DKA/src/main/kotlin/ru/bauman/kompil/App.kt
+++ Lab1/DKA/src/main/kotlin/ru/bauman/kompil/App.kt
@@ -3,7 +3,6 @@ package ru.bauman.kompil
 import ru.bauman.kompil.machine.DFA
 
 fun main() {
-    while(true) {
         println("Введите регулярное выражение: ")
         val regEx = readln()
         val dfa = DFA.of(regEx)
@@ -12,10 +11,11 @@ fun main() {
         val minDFA = DFA.minimization(dfa, "ITMO-ALG")
         println("Таблица минимального конечного автомата: ")
         println(minDFA)
-        println("Введите входную цепочку: ")
-        val inputs = readln()
-        val isReg = dfa.contains(inputs)
-        if (isReg) println("Строка прошла")
-        else println("Не прошла")
-    }
+        while (true) {
+            println("Введите входную цепочку: ")
+            val inputs = readln()
+            val isReg = dfa.contains(inputs)
+            if (isReg) println("Строка прошла")
+            else println("Не прошла")
+        }
 }
